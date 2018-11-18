c0f224c93e929b6f17d11a6f45387277ca2d89d3
Seryozha Khachatryan <serojkhachatryan95@gmail.com>
2018-11-18 20:49:41 +0400

    Added auth guard
    - added auth guard to prevent unauthenticated users from accessing restricted routes.

---
+++ src/app/_guard/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  // canActivate(
+  //   next: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+  //   return true;
+  // }
+
+  // TODO add return value Observable<boolean> | Promise<boolean> | boolean
+  constructor(private router: Router) { }
+  
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (localStorage.getItem('currentUser')) {
+      // logged in so return true
+      return true;
+    }
+
+    // not logged in so redirect to login page with the return url
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
+
+}
